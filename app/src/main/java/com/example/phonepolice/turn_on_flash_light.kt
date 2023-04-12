4b289faf79c6e49e6bd85d4fe924f81553b49e41
Uphar Gaur <uphargaur@gmail.com>
2023-04-12 10:57:11 +0530

    torch chal rhi hai

--- app/src/main/java/com/example/phonepolice/turn_on_flash_light.kt
+++ app/src/main/java/com/example/phonepolice/turn_on_flash_light.kt
@@ -8,6 +8,11 @@ import android.widget.Button
 import android.widget.Switch
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 
 lateinit var  flashcontrol :Button
@@ -20,6 +25,29 @@ class turn_on_flash_light : AppCompatActivity() {
         var cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
         flashcontrol =findViewById(R.id.flashlight_button)
         var flashoff : Button =findViewById(R.id.flashlight_turnoff)
+        //database instialize
+        val db = Firebase.database
+        val usersRef = db.getReference("users")
+        usersRef.child(username).child("Torch").setValue("0")
+        var check:Int =1
+        usersRef.child(username).child("Torch").addValueEventListener(
+            object : ValueEventListener {
+                override fun onDataChange(p0: DataSnapshot) {
+                    check=check+1;
+                    if(check%2==0) {
+                        cameraManager.setTorchMode("0", false)
+                    } else {
+                        cameraManager.setTorchMode("0", true)
+                    }
+
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {}
+
+
+            }
+        )
         flashcontrol.setOnClickListener()
         {
             cameraManager.setTorchMode("0",true)
--- app/src/main/java/com/example/phonepolice/user_tools.kt
+++ app/src/main/java/com/example/phonepolice/user_tools.kt
@@ -63,58 +63,60 @@ class user_tools : AppCompatActivity() {
 
     private fun Savelocation() {
         checkLocationPermission()
-         class MyLocationListener : LocationListener {
-
-            override fun onLocationChanged(location: Location) {
-                // Called when a new location is obtained
-                val latitude = location.latitude
-                val longitude = location.longitude
-                // Do something with the location information
-                val db = Firebase.database
-                val usersRef = db.getReference("users")
-                usersRef.child(username).child("Request").addValueEventListener(
-                    object : ValueEventListener {
-                        override fun onDataChange(p0: DataSnapshot) {
-                            // get datatime
-                            val df = SimpleDateFormat("yyyy/MMM/dd HH:MM:ss")
-                            val date = Date()
-
-                            usersRef.child(username).child(mobileno).child("location").child("lat").setValue(latitude)
-                            usersRef.child(username).child(mobileno).child("location").child("log").setValue(longitude)
-                            usersRef.child(username).child(mobileno).child("location").child("last online").setValue(df.format(date).toString())
-                        }
-
-                        override fun onCancelled(error: DatabaseError) {
-
-                        }
-
-
-                    }
-                )
-            }
-
-            override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
-                // Called when the status of the GPS provider changes
-                // You can handle different status codes here
-            }
-
-            override fun onProviderEnabled(provider: String) {
-                // Called when the GPS provider is enabled
-            }
-
-            override fun onProviderDisabled(provider: String) {
-                // Called when the GPS provider is disabled
-            }
-        }
         //set location baji
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,3,3f, MyLocationListener()
+       //location renew and call by toper and gps
+        val db = Firebase.database
+        val usersRef = db.getReference("users")
+        usersRef.child(username).child("Request").addValueEventListener(
+            object : ValueEventListener {
+                override fun onDataChange(p0: DataSnapshot) {
+                    // Get the last known location from the location manager
+                    checkLocationPermission()
+                    myLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+                    // get datatime
+                    val df = SimpleDateFormat("yyyy/MMM/dd HH:MM:ss")
+                    val date = Date()
+                    println("Onchange location khali wala}")
+
+                    usersRef.child(username).child(mobileno).child("location").child("lat").setValue(myLocation!!.latitude.toString())
+                    usersRef.child(username).child(mobileno).child("location").child("log").setValue(myLocation!!.longitude.toString())
+                    usersRef.child(username).child(mobileno).child("location").child("last online").setValue(df.format(date).toString())
+                }
+
+                override fun onCancelled(error: DatabaseError) {}
+
+
+        }
+
         )
+
+
 
     }
 
 
+    companion object{
+        var myLocation: Location?=null
+        var isServiceRunning=false
+    }
+    inner class MyLocationListener: LocationListener {
+        constructor() : super() {
 
+            myLocation = Location("me")
+            myLocation!!.longitude = 0.1
+            myLocation!!.latitude = 0.6
+        }
+
+        override fun onLocationChanged(location: Location) {
+           println("Onchange location ${location.longitude.toString()}")
+            myLocation=location
+            myLocation!!.latitude=location.latitude
+            myLocation!!.longitude=location.longitude
+        }
+
+
+    }
     }
 
 
